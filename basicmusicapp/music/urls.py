0b7959a97921e9968d90fba79a3e4743a8690148
Pawan yadav <pawan.yadav1107@gmail.com>
2018-01-15 20:36:43 +0530

    added database conn and files

--- basicmusicapp/music/urls.py
+++ basicmusicapp/music/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [  
+	# /music/
     path('', views.index, name= 'index'),
+    # / music/12/
+    path('<int:album_id>/', views.detail, name= 'detail'),
+
 ]
--- basicmusicapp/music/views.py
+++ basicmusicapp/music/views.py
@@ -1,22 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Album
+from django.template import loader
 import pymysql
-def index(request):
-	connection = pymysql.connect(host="localhost", user="root", password="", db="my_python" )
-	 
-
-	try:
-	    with connection.cursor() as cursor:
-	        # Create a new record 
-	        sql = "SELECT * FROM  `users`"
-	        cursor.execute(sql)
-	        print(cursor.description)
-	        print()
-	        for row in cursor: 
-			return HttpResponse("<h1>row</h1>")
-	    connection.commit()    	
-	 
-	finally:
-	    connection.close()
-
+def index(request): 
+	all_albums = Album.objects.all()
+	template = loader.get_template('music/index.html') 
+	context = {
+		'all_albums':all_albums,
+	}
+	return HttpResponse(template.render(context,request)) 
  
+def detail(request, album_id):
+	return HttpResponse("<h2>Details for Album id:"+ str(album_id)+"</h2>")
